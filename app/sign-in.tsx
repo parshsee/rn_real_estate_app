611dc4f4eb612db9df9943b733f2965294ee5353
Parshotan <parshsee@gmail.com>
2026-04-09 19:17:38 -0400

    added redirect to homepage after successful signin

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,6 +1,8 @@
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 import React from "react";
 import {
   Alert,
@@ -13,12 +15,21 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
+
+  // If you are logged in, if you try to go back to sign in page, it will automatically redirect you to the home page
+  if (!loading && isLoggedIn) {
+    return <Redirect href="/" />;
+  }
+
   const handleLogin = async () => {
     const result = await login();
 
     if (result) {
       // Navigate to the home screen
       console.log("Login successful:");
+      // Will also redirect to "/", which is index.tsx
+      refetch(); // Refetch user data to update the global context with the newly logged in user's information
     } else {
       Alert.alert("Login Failed", "Unable to log in. Please try again.");
     }
