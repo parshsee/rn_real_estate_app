2fd3e95443c9eb7e6d12681ff62e112946412682
Parshotan <parshsee@gmail.com>
2026-04-11 22:55:24 -0400

    removed links, now using tabs

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -13,10 +12,6 @@ export default function Index() {
       <Text className="font-bold text-3xl my-10 font-rubik ">
         Welcome to Real Estate App
       </Text>
-      <Link href="/sign-in"> Sign In</Link>
-      <Link href="/explore"> Explore</Link>
-      <Link href="/profile"> Profile</Link>
-      <Link href="/properties/123"> Property</Link>
     </View>
   );
 }
