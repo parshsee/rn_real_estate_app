e1cd38ee4dcb27fbf17fb08474d43e847aa801f6
Parshotan <parshsee@gmail.com>
2026-04-16 18:48:45 -0400

    Created basic header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,31 @@
-import { Text, View } from "react-native";
+import Search from "@/components/search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10 font-rubik ">
-        Welcome to Real Estate App
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-4">
+      <View className="px-5">
+        {/* View for positioning our elements*/}
+        <View className="flex flex-row items-center justify-between mt-5">
+          {/* View for the header */}
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Parshotan
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+      <Search />
+    </SafeAreaView>
   );
 }
