ae1a2c17da38fd8c3de1d4d266a9451f3bb223df
Parshotan <parshsee@gmail.com>
2026-04-16 19:36:46 -0400

    added code for featured header and cards, moved search into padding view

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,14 +1,15 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       <View className="px-5">
-        {/* View for positioning our elements*/}
+        {/* Header */}
         <View className="flex flex-row items-center justify-between mt-5">
           {/* View for the header */}
           <View className="flex flex-row">
@@ -24,8 +25,26 @@ export default function Index() {
           </View>
           <Image source={icons.bell} className="size-6" />
         </View>
+        {/* Search bar */}
+        <Search />
+        {/* Featured Section */}
+        {/* Header */}
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* Featured Card */}
+        <FeaturedCard />
+        <Card />
       </View>
-      <Search />
     </SafeAreaView>
   );
 }
