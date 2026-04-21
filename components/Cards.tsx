218639afa02a6cd89ddcdbd3b0af818286e03edf
Parshotan <parshsee@gmail.com>
2026-04-21 18:42:57 -0400

    Updated cards to dynamically use property information from DB call

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,22 +2,27 @@ import icons from "@/constants/icons";
 import images from "@/constants/images";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Models } from "react-native-appwrite";
 
 // Reusable component for displaying cards, used in index and product details
 
 interface Props {
+  item: Models.DefaultRow; // Uses Row Model instead of Document Model, since Appwrite is Table not Collections anymore
   onPress?: () => void; // Optional function that returns nothing
 }
 
 // Component for the featured cards
-export const FeaturedCard = ({ onPress }: Props) => {
+export const FeaturedCard = ({
+  item: { image, rating, name, address, price },
+  onPress,
+}: Props) => {
   return (
     <TouchableOpacity
       onPress={onPress}
       className="flex flex-col items-start w-60 h-80 relative"
     >
       {/* Japan image */}
-      <Image source={images.japan} className="size-full rounded-2xl" />
+      <Image source={{ uri: image }} className="size-full rounded-2xl" />
       {/* Have a gradient over the image */}
       <Image
         source={images.cardGradient}
@@ -27,7 +32,7 @@ export const FeaturedCard = ({ onPress }: Props) => {
       <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5 ">
         <Image source={icons.star} className="size-3.5" />
         <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
-          4.4
+          {rating}
         </Text>
       </View>
 
@@ -38,17 +43,15 @@ export const FeaturedCard = ({ onPress }: Props) => {
           className="text-xl font-rubik-extrabold text-white"
           numberOfLines={1}
         >
-          Modern Apartment
+          {name}
         </Text>
-        <Text className="text-base font-rubik text-white">
-          22 W 15th St, New York, NY 10011
-        </Text>
+        <Text className="text-base font-rubik text-white">{address}</Text>
 
         {/* Render elements in a row */}
         <View className="flex flex-row items-center justify-between w-full">
           {/* Show  the price and a heart icon for saving*/}
           <Text className="text-xl font-rubik-extrabold text-white">
-            $2,500
+            ${price}
           </Text>
           <Image source={icons.heart} className="size-5" />
         </View>
@@ -58,7 +61,10 @@ export const FeaturedCard = ({ onPress }: Props) => {
 };
 
 // Component for the regular cards
-export const Card = ({ onPress }: Props) => {
+export const Card = ({
+  item: { image, rating, name, address, price },
+  onPress,
+}: Props) => {
   // TODO: Fix error  ERROR  Text strings must be rendered within a <Text> component. Says line 72?
 
   return (
@@ -70,26 +76,22 @@ export const Card = ({ onPress }: Props) => {
       <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
         <Image source={icons.star} className="size-2.5" />
         <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
-          4.4
+          {rating}
         </Text>
       </View>
       {/* New York image */}
-      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+      <Image source={{ uri: image }} className="w-full h-40 rounded-lg" />
       {/* Render elements in a column from the bottom up, on top of the image */}
       <View className="flex flex-col mt-2">
         {/* Show the name of the card and address */}
-        <Text className="text-base font-rubik-bold text-black-300">
-          Cozy Studio
-        </Text>
-        <Text className="text-xs font-rubik text-black-200">
-          22 W 15th St, New York, NY 10011
-        </Text>
+        <Text className="text-base font-rubik-bold text-black-300">{name}</Text>
+        <Text className="text-xs font-rubik text-black-200">{address} </Text>
 
         {/* Render elements in a row */}
         <View className="flex flex-row items-center justify-between mt-2">
           {/* Show  the price and a heart icon for saving*/}
           <Text className="text-base font-rubik-bold text-primary-300">
-            $2,500
+            ${price}
           </Text>
           <Image
             source={icons.heart}
