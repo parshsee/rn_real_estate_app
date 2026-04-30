f4b374d7608c376e9d894ceb47a5fd97c01e1bbe
Parshotan <parshsee@gmail.com>
2026-04-30 18:47:34 -0400

    Added code to show agent info

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -55,7 +55,7 @@ const Property = () => {
           </View>
         </View>
       </View>
-      {/* Property Basic Info */}
+      {/* Property Info and Agent Info */}
       <View className="px-5 mt-7 flex gap-2">
         {/* Property title */}
         <Text className="text-2xl font-rubik-extrabold">Modern Apartment</Text>
@@ -102,9 +102,37 @@ const Property = () => {
             1500 sqft
           </Text>
         </View>
-      </View>
 
-      {/* Display agent information */}
+        {/* Display agent information */}
+        <View className="w-full border-t border-primary-200 pt-7 mt-5">
+          <Text className="text-black-300 text-xl font-rubik-bold">Agent</Text>
+        </View>
+
+        {/* Display agent information in a row */}
+        <View className="flex flex-row items-center justify-between mt-4">
+          {/* Show agent avatar, name, and email on left side*/}
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-14 rounded-full" />
+
+            <View className="flex flex-col items-start justify-center ml-3">
+              <Text className="text-lg text-black-300 text-start font-rubik-bold">
+                Agent Name
+              </Text>
+              <Text className="text-sm text-black-300 text-start font-rubik-medium">
+                agent@gmail.com
+              </Text>
+            </View>
+          </View>
+
+          {/* Show message and phone icons on right side */}
+          <View className="flex flex-row items-center gap-3">
+            <Image source={icons.chat} className="size-7" />
+            <Image source={icons.phone} className="size-7" />
+          </View>
+        </View>
+
+        {/* Display property overview */}
+      </View>
     </View>
   );
 };
