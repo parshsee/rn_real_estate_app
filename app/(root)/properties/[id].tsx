cab450084d911df67a1af7606b17780125bf128e
Parshotan <parshsee@gmail.com>
2026-05-02 17:07:32 -0400

    Updated property and agent info using data from db

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,5 +1,7 @@
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { getPropertyById } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
 import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
 import {
@@ -13,9 +15,22 @@ import {
 } from "react-native";
 
 const Property = () => {
-  const { id } = useLocalSearchParams();
+  // Get specifically the id parameter from the URL using useLocalSearchParams hook
+  const { id } = useLocalSearchParams<{ id?: string }>();
 
   const windowHeight = Dimensions.get("window").height;
+
+  const { data: property } = useAppwrite({
+    fn: getPropertyById,
+    params: {
+      id: id!,
+    },
+  });
+
+  // console.log(property);
+  console.log(property?.agent);
+  // console.log(property?.gallery);
+  // console.log(property?.reviews);
 
   return (
     <View>
@@ -26,7 +41,7 @@ const Property = () => {
         {/* Header section with the property image and navigation icons */}
         <View className="relative w-full" style={{ height: windowHeight / 2 }}>
           <Image
-            source={images.newYork}
+            source={{ uri: property?.image }}
             className="size-full"
             resizeMode="cover"
           />
@@ -64,21 +79,21 @@ const Property = () => {
         <View className="px-5 mt-7 flex gap-2">
           {/* Property title */}
           <Text className="text-2xl font-rubik-extrabold">
-            Modern Apartment
+            {property?.name}
           </Text>
 
           {/* Property type and rating */}
           <View className="flex flex-row items-center gap-3">
             <View className="flex flex-row items-center px-4 py-2 bg-primary-100 rounded-full">
               <Text className="text-xs font-rubik-bold text-primary-300">
-                Condo
+                {property?.type}
               </Text>
             </View>
 
             <View className="flex flex-row items-center gap-2">
               <Image source={icons.star} className="size-5" />
               <Text className="text-black-200 text-sm mt-1 font-rubik-medium">
-                4.5 (5000 reviews)
+                {property?.rating} ({property?.reviews?.length} reviews)
               </Text>
             </View>
           </View>
@@ -90,7 +105,7 @@ const Property = () => {
               <Image source={icons.bed} className="size-4" />
             </View>
             <Text className="text-black-300 text-sm font-rubik-medium ml-2">
-              5 Beds
+              {property?.bedrooms} Beds
             </Text>
 
             {/* Display bath icon and number of baths */}
@@ -98,7 +113,7 @@ const Property = () => {
               <Image source={icons.bath} className="size-4" />
             </View>
             <Text className="text-black-300 text-sm font-rubik-medium ml-2">
-              5 Baths
+              {property?.bathrooms} Baths
             </Text>
 
             {/* Display area icon and sqft */}
@@ -106,7 +121,7 @@ const Property = () => {
               <Image source={icons.area} className="size-4" />
             </View>
             <Text className="text-black-300 text-sm font-rubik-medium ml-2">
-              1500 sqft
+              {property?.area} sqft
             </Text>
           </View>
 
@@ -121,14 +136,17 @@ const Property = () => {
           <View className="flex flex-row items-center justify-between mt-4">
             {/* Show agent avatar, name, and email on left side*/}
             <View className="flex flex-row items-center">
-              <Image source={images.avatar} className="size-14 rounded-full" />
+              <Image
+                source={{ uri: property?.agent?.avatar }}
+                className="size-14 rounded-full"
+              />
 
               <View className="flex flex-col items-start justify-center ml-3">
                 <Text className="text-lg text-black-300 text-start font-rubik-bold">
-                  Agent Name
+                  {property?.agent?.name}
                 </Text>
                 <Text className="text-sm text-black-300 text-start font-rubik-medium">
-                  agent@gmail.com
+                  {property?.agent?.email}
                 </Text>
               </View>
             </View>
@@ -146,9 +164,17 @@ const Property = () => {
               Overview
             </Text>
             <Text className="text-black-200 text-base font-rubik mt-2">
-              This is a modern apartment with a beautiful view. It has 5
-              bedrooms and 5 bathrooms.
+              {property?.description}
             </Text>
+          </View>
+
+          {/* Display property facilities */}
+          <View className="mt-7">
+            <Text className="text-black-300 text-xl font-rubik-bold">
+              Facilities
+            </Text>
+
+            {/* TODO: Add facility icons and names */}
           </View>
         </View>
       </ScrollView>
