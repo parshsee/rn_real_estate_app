717d77b72941d9f5b47d3f0911f428c9904d56ab
Parshotan <parshsee@gmail.com>
2026-05-03 16:44:06 -0400

    Added code to show fake location, just static image

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -30,7 +30,6 @@ const Property = () => {
   });
 
   // console.log(property);
-  console.log(property?.gallery);
   // console.log(property?.reviews);
 
   return (
@@ -242,6 +241,24 @@ const Property = () => {
               contentContainerStyle={{ paddingRight: 20 }}
             />
           )}
+
+          {/* Display location */}
+          <View className="mt-7">
+            <Text className="text-black-300 text-xl font-rubik-bold">
+              Location
+            </Text>
+            {/* Location icon and address */}
+            <View className="flex flex-row items-center justify-start mt-4 gap-2">
+              <Image source={icons.location} className="w-7 h-7" />
+              <Text className="text-black-200 text-sm font-rubik-medium">
+                {property?.address}
+              </Text>
+            </View>
+            <Image
+              source={images.map}
+              className="h-52 w-full mt-5 rounded-xl"
+            />
+          </View>
         </View>
       </ScrollView>
     </View>
